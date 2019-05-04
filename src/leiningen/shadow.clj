148d0e219c2fc8d76fb48bba24c36689f0e375b3
Nikola Peric <nik@nikperic.com>
2019-05-04 18:37:38 -0400

    More attempts to get it working with jdk9+

--- src/leiningen/shadow.clj
+++ src/leiningen/shadow.clj
@@ -5,7 +5,10 @@
             [shadow.cljs.devtools.api :as api]
             [shadow.cljs.devtools.config :as config]
             [shadow.cljs.devtools.cli-opts :as cli-opts]
-            [shadow.cljs.devtools.server :as server]))
+            [shadow.cljs.devtools.server :as server]
+            [shadow.cljs.devtools.server.runtime :as runtime]
+            [shadow.runtime.services :as rt]
+            [shadow.cljs.devtools.server.common :as common]))
 
 (defn read-config
   "Uses shadow-cljs' config pipeline to ensure config matches default slurped shadow-cljs.edn"
@@ -25,15 +28,46 @@
        (map name)
        (string/join ", ")))
 
+
+;; Hack in config to make-runtime
+(defn make-runtime [config]
+  (let [config (-> config
+                   ;; just in case someone gets the idea to put :server-runtime true into their config
+                   (dissoc :server-runtime))]
+
+    (-> {::started (System/currentTimeMillis)
+         :config config}
+        (rt/init (common/get-system-config config))
+        (rt/start-all)
+        )))
+
+(defmacro with-configed-runtime [config & body]
+  ;; not using binding since there should only ever be one runtime instance per JVM
+  `(let [body-fn#
+         (fn []
+           ~@body)]
+     (if (runtime/get-instance)
+       (body-fn#)
+
+       ;; start/stop instance when not running in server context
+       (let [runtime# (make-runtime ~config)]
+         (try
+           (runtime/set-instance! runtime#)
+           (body-fn#)
+           (finally
+             (runtime/reset-instance!)
+             (rt/stop-all runtime#)))))))
+;; End hack
+
+
 (defn run-shadow-cljs!
   "Execute shadow-cljs programmatically"
   [config-map command build & rest-args]
-  (let [server-config (dissoc config-map :builds)
-        opts          (:options (cli/parse-opts rest-args cli-opts/cli-spec))]
+  (let [opts (:options (cli/parse-opts rest-args cli-opts/cli-spec))]
     (case command
       ;; server commands
       "start"
-      (server/start! server-config)
+      (server/start! config-map)
       "stop"
       (server/stop!)
       "reload"
@@ -41,7 +75,8 @@
 
       (if-let [build-config (get-in config-map [:builds (keyword build)])]
         ;; build commands
-        (api/with-runtime
+        (with-configed-runtime
+          config-map
           (case command
             "compile"
             (api/compile* build-config opts)
@@ -54,6 +89,9 @@
             ;; default:
             (lein/warn "Invalid command. Please use one of 'compile', 'release', 'watch', or 'check'")))
         (lein/warn (str "Unable to find build " build ". Currently valid builds are: " (valid-builds config-map)))))))
+
+;; TODO: package.json generation
+;; TODO: npm install call?
 
 (defn shadow
   "Uses the :shadow-cljs key from your project.clj file as shadow-cljs config, separate from shadow-cljs.edn.
