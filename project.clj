39ce5d66c5bc8dc432a809d2389ee48afe3ca94b
Isaac Johnston <isaac@superstruct.nz>
2020-08-07 01:07:06 +1200

    Fix npm integration
    In short, this change makes npm integration behave in the way one would
    reasonably expect regardless of using a `package.json` file or cleaning
    out `package.json` between each build by only using `deps.cljs` files
    etc.
    
    1. If a `package.json` file does not exist the first `npm install
    --save...` command executed will, under certain conditions, return
    a successful exit code and create `package.json` BUT not actually
    install the packages in the `node_modules` directory. To fix this,
    lein-shadow now always creates an empty `package.json` if one does not
    already exist as it is a common practice in lein-shadow projects to
    put `package.json` in `.gitignore`.
    
    2. If a `package.json` file does exist and has some dependencies or
    devDependencies, then lein-shadow will now execute `npm install` prior
    to anything else. shadow-cljs only executes `npm install` for the specific
    packages in transitive dependencies (i.e. `deps.cljs` files on the
    classpath), and not for anything else that might be in `package.json`.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://gitlab.com/nikperic/lein-shadow"
   :license {:name "MIT License"
             :url  "https://opensource.org/licenses/MIT"}
-  :dependencies [[meta-merge "1.0.0"]]
+  :dependencies [[meta-merge            "1.0.0"]
+                 [org.clojure/data.json "1.0.0"]]
   :eval-in-leiningen true)
